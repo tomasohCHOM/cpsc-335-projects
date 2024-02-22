3eb886c0825184a140ff4dc24a66c4646629d26e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-02-21 18:08:00 -0800

    Solved greedy quesetion

--- project-01/greedy.py
+++ project-01/greedy.py
@@ -3,28 +3,29 @@ from parse import parse_list
 
 
 def greedy_hamiltonian(distances: list[int], fuel: list[int], mpg: int) -> int:
-    # Initialize global_max and local_max to 0
-    global_max, local_max = 0, 0
+    # Initialize surplus to 0. This will indicate how
+    # much gas is left in our tank to travel around cities
+    surplus = 0
     # Initialize start to 0. This will be our returning value
     start = 0
 
     # Loop through distances array, keeping track of index
     for i in range(len(distances)):
         # Update global_max and local_max
-        global_max += distances[i]
-        local_max += distances[i]
+        surplus += fuel[i] * mpg - distances[i]
 
-        # If local_max is less than 0, we know we cannot
+        # If surplus is less than 0, we know we cannot
         # reach our destination from our starting point
         # because we will not have enough gas. Instead, start
-        # from the city next to it (i + 1)
-        if local_max < 0:
-            local_max = 0
+        # from the city next to it (i + 1). We do not worry about
+        # circling around (starting at city with index = 0) because
+        # we already looped through it
+        if surplus <= 0:
+            surplus = 0
             start = i + 1
 
     # return the starting index of the preferred starting city
-    # or -1 if it is not possible
-    return start if global_max >= 0 else -1
+    return start
 
 
 # Define the main process when this file is run
